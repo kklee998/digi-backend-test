e6aefed88fbf98e16ee9fe45f5c7c34657282cec
kklee998 <kahkinlee998@gmail.com>
2021-02-13 04:18:48 +0800

    fixed q typo

--- handlers.js
+++ handlers.js
@@ -36,7 +36,7 @@ const userListHandler = async (c, req, res) => {
       .from('users')
       .modify((qb) => {
         if (q) {
-          qb.where('name', 'ILIKE', q);
+          qb.where('name', 'ILIKE', `%${q}%`);
         }
       });
     return res.status(200).json(userList);
@@ -110,7 +110,7 @@ const userLoginHandler = async (c, req, res) => {
   }
   try {
     const { email, password } = body;
-    const [user] = await knex.table('users').select('name', 'password').where('email', 'ILIKE', `%${email}%`);
+    const [user] = await knex.table('users').select('name', 'password').where('email', '=', email);
     if (!user) {
       return res.status(400).json(errorFormatter('2', 'Invalid login', 'The provided email/password is incorrect'));
     }
