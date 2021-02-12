43193727f020c78ab7fece67c54922e9b0dc2681
kklee998 <kahkinlee998@gmail.com>
2021-02-12 21:50:14 +0800

    added ssl to db url

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,20 @@
 const inProduction = process.env.NODE_ENV === 'production';
-module.exports = {
-
-  client: 'pg',
-  connection: process.env.DATABASE_URL || {
+let DB_URL = process.env.DATABASE_URL;
+if (DB_URL) {
+  DB_URL = `${DB_URL}?ssl=true`;
+} else {
+  DB_URL = {
     host: 'localhost',
     port: 5432,
     user: 'postgres',
     password: 'postgres',
     database: 'dev',
-  },
+  };
+}
+module.exports = {
+
+  client: 'pg',
+  connection: DB_URL,
   ssl: !!inProduction,
   migrations: {
     directory: './db/migrations',
